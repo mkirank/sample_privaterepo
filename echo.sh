29d7b474a6d4e418cd37f674a5277a5a66c86c30
shiptest-rc-ow <shiptest.rc.ow@gmail.com>
2017-02-13 15:33:47 +0530

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -1,22 +1,22 @@
 #!/bin/bash -e
 
-#export RES_REPO=sample_pvt_pr_test_reports
+export RES_REPO=sample_pvt_pr_test_reports
 
-#export RES_REPO_UP=$(echo ${RES_REPO//-/} | awk '{print toupper($0)}')
+export RES_REPO_UP=$(echo ${RES_REPO//-/} | awk '{print toupper($0)}')
 
-#set_context() {
- # export VERSION=$(eval echo "$"$RES_VER_UP"_VERSIONNAME")
-#}
+set_context() {
+  export VERSION=$(eval echo "$"$RES_REPO_UP"_VERSIONNAME")
+}
 
 #sl
 echo 'Hello scriptprivate project'     
 #sleep 1m  
 echo 'slept well'
 #env
-#echo "$VERSION"
+echo "$VERSION"
 
-#main() {
-#  set_context
-#}
+main() {
+  set_context
+}
 
-#main
+main
