94cc3dbee2f453996f3761dac44e48471ed3e412
shiptest-rc-ow <shiptest.rc.ow@gmail.com>
2017-02-13 17:01:46 +0530

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -1,18 +1,5 @@
 #!/bin/bash -e
 
-export CURR_MY_JOB=runShtest1
-echo $CURR_MY_JOB
-export CURR_MY_JOB_UP=$(echo ${CURR_MY_JOB//-/} | awk '{print toupper($0)}')
-echo $CURR_MY_JOB_UP
-
-export CURR_MY_JOB_VERSION=$(eval echo "$"$CURR_MY_JOB_UP"_VERSIONNUMBER")
-  
-echo $CURR_MY_JOB_VERSION
-
-
 #sl
 echo 'Hello scriptprivate project'     
 #sleep 1m  
-
-env
-
