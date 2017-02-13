f821f11ea024cc38d7ced161a7ea919dc12a64c9
shiptest-rc-ow <shiptest.rc.ow@gmail.com>
2017-02-13 15:59:55 +0530

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -1,24 +1,24 @@
 #!/bin/bash -e
 
-export RESO_JOB=runShtest1
+#export RESO_JOB=runShtest1
 
-export RESO_JOB_UP=$(echo ${RESO_JOB//-/} | awk '{print toupper($0)}')
+#export RESO_JOB_UP=$(echo ${RESO_JOB//-/} | awk '{print toupper($0)}')
 
-set_context() {
-  export VERSION=$(eval echo "$"$RESO_JOB_UP"_VERSIONNUMBER")
-  echo "$VERSION"
-  echo "testing in set_context"
-}
+#set_context() {
+#  export VERSION=$(eval echo "$"$RESO_JOB_UP"_VERSIONNUMBER")
+#  echo "$VERSION"
+ # echo "testing in set_context"
+#}
 
 #sl
-#echo 'Hello scriptprivate project'     
+echo 'Hello scriptprivate project'     
 #sleep 1m  
-#echo 'slept well'
+echo 'slept well'
 #env
 
 
-main() {
-  set_context
-}
+#main() {
+#  set_context
+#}
 
-main
+#main
