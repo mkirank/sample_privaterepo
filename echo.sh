5b5b3a2006ba871e709c10dc281d6a94990d7031
shiptest-rc-ow <shiptest.rc.ow@gmail.com>
2017-02-13 15:43:58 +0530

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -5,7 +5,7 @@ export RESO_JOB=runShtest1
 export RESO_JOB_UP=$(echo ${RESO_JOB//-/} | awk '{print toupper($0)}')
 
 set_context() {
-  export VERSION=$(eval echo "$"$RESO_JOB_UP"_VERSIONNAME")
+  export VERSION=$(eval echo "$"$RESO_JOB_UP"_VERSIONNUMBER")
   echo "$VERSION"
   echo "testing in set_context"
 }
