b4b6f92fdd5877234c5a430219f3d26f74032d44
shiptest-rc-ow <shiptest.rc.ow@gmail.com>
2017-02-13 16:14:47 +0530

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -3,7 +3,7 @@
 export CURR_MY_JOB=runShtest1
 echo $CURR_MY_JOB
 export CURR_MY_JOB_UP=$(echo ${CURR_MY_JOB//-/} | awk '{print toupper($0)}')
-
+echo $CURR_MY_JOB_UP
 set_context() {
   export CURR_MY_JOB_VERSION=$(eval echo "$"$curr_my_job"_VERSIONNUMBER")
   
