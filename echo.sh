7f83d61fafc11f1da6f1ef6083cad780f6e954b2
shiptest-rc-ow <shiptest.rc.ow@gmail.com>
2017-02-20 18:01:19 +0530

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -8,8 +8,8 @@ echo ' ---> one'
 export IMG_RES="image"
 export REPO_RES="run2"
 
-export IMG_RES_UP=$(echo $IMG_RES | awk '{print toupper($0)}'
-export REPO_RES_UP=$(echo $REPO_RES | awk '{print toupper($0)}'
+export IMG_RES_UP=$(echo $IMG_RES | awk '{print toupper($0)}')
+export REPO_RES_UP=$(echo $REPO_RES | awk '{print toupper($0)}')
 
 export IMG_VERSION=$(eval echo "$"$IMG_RES_UP"_VERSIONNUMBER")
 export IMG_VERSION_AS_REPOSVER=$(eval echo "$"$REPO_RES_UP"_VERSIONNUMBER")
