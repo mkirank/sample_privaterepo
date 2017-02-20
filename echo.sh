05a57fbeb311be9705ec9740234c5b8650e65fae
shiptest-rc-ow <shiptest.rc.ow@gmail.com>
2017-02-20 18:05:30 +0530

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -14,5 +14,8 @@ export REPO_RES_UP=$(echo $REPO_RES | awk '{print toupper($0)}')
 export IMG_VERSION=$(eval echo "$"$IMG_RES_UP"_VERSIONNUMBER")
 export IMG_VERSION_AS_REPOSVER=$(eval echo "$"$REPO_RES_UP"_VERSIONNUMBER")
 
+echo img_verion=$IMG_VERSION
+echo img_version_as_repo=$IMG_VERSION_AS_REPOSVER
+
 echo "versionName=$IMG_VERSION_AS_REPOSVER" > /build/state/$IMG_RES.env #adding version state
 echo "anotherProperty=$IMG_VERSION" >> /build/state/$IMG_RES.env
