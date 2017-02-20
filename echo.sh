e318c8023bc2979812b632e00d780fb06ad13889
shiptest-rc-ow <shiptest.rc.ow@gmail.com>
2017-02-20 17:57:11 +0530

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -3,5 +3,16 @@
 #sl
 echo '--->Hello scriptprivate project' 
 
-sleep 1m  
+#sleep 1m  
 echo ' ---> one'
+export IMG_RES="image"
+export REPO_RES="run2"
+
+export IMG_RES_UP=$(echo $IMG_RES | awk '{print toupper($0)}'
+export REPO_RES_UP=$(echo $REPO_RES | awk '{print toupper($0)}'
+
+export IMG_VERSION=$(eval echo "$"$IMG_RES_UP"_VERSIONNUMBER")
+export IMG_VERSION_AS_REPOSVER=$(eval echo "$"$REPO_RES_UP"_VERSIONNUMBER")
+
+echo "versionName=$IMG_VERSION_AS_REPOSVER" > /build/state/$IMG_RES.env #adding version state
+echo "anotherProperty=$IMG_VERSION" >> /build/state/$IMG_RES.env
